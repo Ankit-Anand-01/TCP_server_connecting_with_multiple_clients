efd509e9c20a37c801c2616b3a1dfb327914f345
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-02-06 15:47:52 +0530

    Add files via upload

---
+++ server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	//start the server
+	ln, err := net.Listen("tcp", ":8080")
+	//error handling
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//closing the listener
+	defer ln.Close()
+	for {
+		//Accept waits and returns the next listener
+		c, err := ln.Accept()
+		//error handling
+		if err != nil {
+			fmt.Println(err)
+			return
+
+		}
+
+		go handleClientRequest(c)
+
+	}
+}
+
+func handleClientRequest(con net.Conn) {
+	defer con.Close()
+
+	cr := bufio.NewReader(con)
+
+	for {
+		//reading the string
+		netData, err := cr.ReadString('\n')
+		//error handling
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//closing the connection
+		if strings.TrimSpace(netData) == "close" {
+			fmt.Println("closed")
+			return
+		}
+
+		fmt.Print(string(netData))
+		//reader := bufio.NewReader(os.Stdin)
+		//fmt.Print("message sent: ")
+		//text, _ := reader.ReadString('\n')
+		//fmt.Fprintf(c, text+"\n")
+	}
+}
